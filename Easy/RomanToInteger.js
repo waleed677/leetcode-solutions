ab59c08f860e5db8b2d1f06ef29713801c7ef038
Waleed-Arshad-com <waleed@computan.net>
2021-11-25 22:43:05 +0500

    leetcode Roman To Integer

---
+++ Easy/RomanToInteger.js
@@ -0,0 +1,31 @@
+let input = "MCMXCIV";
+let output = romanToInt(input);
+console.log(output);
+
+function romanToInt(inp) {
+    let result = 0;
+    for (let i = 0; i < inp.length; i++) {
+        let curr = checkRoman(inp[i]);
+        let next = checkRoman(inp[i + 1]);
+        if (curr < next) {
+            result -= curr;
+        } else {
+            result += curr;
+        }
+    }
+    return result;
+}
+
+
+function checkRoman(s) {
+    let map = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    return map[s];
+}
