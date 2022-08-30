b43cdbe6bcefa5dc5bef20cb512ff616215b7a46
Waleed Arshad Awan <waleedmalik677@gmail.com>
2022-08-30 20:31:08 +0500

    LeetCode Problem # 27

---
+++ Easy/leetcode27.js
@@ -0,0 +1,24 @@
+
+let nums = [3,2,2,3];
+let val = 3;
+
+var removeElement = function(nums, val) {
+    const expectedNums = nums.filter((item) => {
+       return item !== val;
+   });
+   console.log({expectedNums});
+
+   if (expectedNums.length > 0) {
+       nums.splice(0, nums.length - expectedNums.length);
+       for (let i = 0 ; i < expectedNums.length ; i++) {
+           nums[i] = expectedNums[i];
+       }
+   } else {
+        nums.splice(0,nums.length);
+   } 
+
+
+        console.log(nums);
+};
+
+removeElement(nums, val);
